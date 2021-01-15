d423a6b104eb4946616c948ed3bbdab81aee2b17
barti132 <barteksredzinski@gmail.com>
2021-01-15 02:28:41 +0100

    multi kill for computer player

--- src/main/java/pl/barti/AI.java
+++ src/main/java/pl/barti/AI.java
@@ -30,66 +30,100 @@ public class AI{
             if(p.getType() == PieceType.WHITE)
                 continue;
 
+            boolean flag = false;
             int x = p.getX();
             int y = p.getY();
             int left = x - 1;
             int right = x + 1;
             int up = y - 1;
             int down = y + 1;
-            //w góre
-            if(p.isKing()){
-                if(left >= 0 && up >= 0){
-                    if(board[left][up].hasPiece() && board[left][up].getPiece().getType() == PieceType.WHITE && x - 2 >= 0 && y - 2 >= 0 && !board[x - 2][y - 2].hasPiece()){
-                        pieceList.add(board[left][up].getPiece());
-                        killOne(board, x, y, x - 2, y - 2, left, up);
-                        break;
-                    }
-                    else if(!board[left][up].hasPiece()){
-                        move(board, x, y, left, up);
-                        break;
-                    }
+
+            while(true){
+                x = p.getX();
+                y = p.getY();
+                left = x - 1;
+                right = x + 1;
+                up = y - 1;
+                down = y + 1;
+
+                if(p.isKing() && left >= 0 && up >= 0 && checkLeftUp(board, x, y)){
+                    pieceList.add(board[left][up].getPiece());
+                    killOne(board, x, y, x - 2, y - 2, left, up);
                 }
-                if(right < Game.WIDTH && up >= 0){
-                    if(board[right][up].hasPiece() && board[right][up].getPiece().getType() == PieceType.WHITE && x + 2 < Game.WIDTH && y - 2 >= 0 && !board[x + 2][y - 2].hasPiece()){
-                        pieceList.add(board[right][up].getPiece());
-                        killOne(board, x, y, x + 2, y - 2, right, up);
-                        break;
-                    }
-                    else if(!board[right][up].hasPiece()){
-                        move(board, x, y, right, up);
-                        break;
-                    }
+                else if(p.isKing() && right < Game.WIDTH && up >= 0 && checkRightUp(board, x, y)){
+                    pieceList.add(board[right][up].getPiece());
+                    killOne(board, x, y, x + 2, y - 2, right, up);
                 }
-            }
-            //w dół
-            //lewo
-            if(left >= 0 && down < Game.HEIGHT){
-                if(board[left][down].hasPiece() && board[left][down].getPiece().getType() == PieceType.WHITE && x - 2 >= 0 && y + 2 < Game.HEIGHT && !board[x - 2][y + 2].hasPiece()){
+                else if(left >= 0 && down < Game.HEIGHT && checkLeftDown(board, x, y)){
                     pieceList.add(board[left][down].getPiece());
                     killOne(board, x, y, x - 2, y + 2, left, down);
-                    break;
                 }
-                else if(!board[left][down].hasPiece()){
-                    move(board, x, y, left, down);
-                    break;
-                }
-            }
-            //prawo
-            if(right < Game.WIDTH && down < Game.HEIGHT){
-                if(board[right][down].hasPiece() && board[right][down].getPiece().getType() == PieceType.WHITE && x + 2 < Game.WIDTH && y + 2 < Game.HEIGHT && !board[x + 2][y + 2].hasPiece()){
+                else if(right < Game.WIDTH && down < Game.HEIGHT && checkRightDown(board, x, y)){
                     pieceList.add(board[right][down].getPiece());
                     killOne(board, x, y, x + 2, y + 2, right, down);
+
+                }
+                else{
                     break;
                 }
-                else if(!board[right][down].hasPiece()){
-                    move(board, x, y, right,down);
-                    break;
-                }
+                flag = true;
+            }
+
+            if(flag)
+                break;
+
+            if(p.isKing() && left >= 0 && up >= 0 && !board[left][up].hasPiece()){
+                move(board, x, y, left, up);
+                break;
+            }
+            if(p.isKing() && right < Game.WIDTH && up >= 0 && !board[right][up].hasPiece()){
+                move(board, x, y, right, up);
+                break;
+            }
+            if(left >= 0 && down < Game.HEIGHT && !board[left][down].hasPiece()){
+                move(board, x, y, left, down);
+                break;
+            }
+            if(right < Game.WIDTH && down < Game.HEIGHT && !board[right][down].hasPiece()){
+                move(board, x, y, right,down);
+                break;
             }
         }
 
         playerMove.set(true);
         return pieceList;
+    }
+
+    private boolean checkLeftUp(Tile[][] board, int x, int y){
+        int left = x - 1;
+        int up = y - 1;
+        if(board[left][up].hasPiece() && board[left][up].getPiece().getType() == PieceType.WHITE && x - 2 >= 0 && y - 2 >= 0 && !board[x - 2][y - 2].hasPiece())
+            return true;
+        return false;
+    }
+
+    private boolean checkRightUp(Tile[][] board, int x, int y){
+        int up = y - 1;
+        int right = x + 1;
+        if(board[right][up].hasPiece() && board[right][up].getPiece().getType() == PieceType.WHITE && x + 2 < Game.WIDTH && y - 2 >= 0 && !board[x + 2][y - 2].hasPiece())
+            return true;
+        return false;
+    }
+
+    private boolean checkLeftDown(Tile[][] board, int x, int y){
+        int left = x - 1;
+        int down = y + 1;
+        if(board[left][down].hasPiece() && board[left][down].getPiece().getType() == PieceType.WHITE && x - 2 >= 0 && y + 2 < Game.HEIGHT && !board[x - 2][y + 2].hasPiece())
+            return true;
+        return false;
+    }
+
+    private boolean checkRightDown(Tile[][] board, int x, int y){
+        int right = x + 1;
+        int down = y + 1;
+        if(board[right][down].hasPiece() && board[right][down].getPiece().getType() == PieceType.WHITE && x + 2 < Game.WIDTH && y + 2 < Game.HEIGHT && !board[x + 2][y + 2].hasPiece())
+            return true;
+        return false;
     }
 
     private void killOne(Tile[][] board, int x, int y, int nextX, int nextY, int enemyX, int enemyY){
